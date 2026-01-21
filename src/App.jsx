62de4a08f2e49a3ad2141ae1c31a4aaad5e723de
delhiadelyne <udelhiadelyne2020@gmail.com>
2026-01-21 05:58:18 -0800

    Updated main.jsx and App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function App() {
+  return (
+    <div>
+      <h1>EduChoice</h1>
+      <p>Helping parents choose the right school with confidence.</p>
+    </div>
+  );
+}
+
+export default App;
+
